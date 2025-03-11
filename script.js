fff5b73fb14f0970b5c704b549b53bebe14bc192
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-11 15:14:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-let arr = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 2];
 function incrementArray(arr) {
   //your code here
+	let newArr = [];
+	for(let num of arr){
+		newArr.push(num+1)
+}
+	return newArr
 }
 
 alert(incrementArray(arr));
